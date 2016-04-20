83e8e0d54219c45325ac736321cb4e07e07d2e6e
Clay Miller <clay@smockle.com>
2016-04-20 11:19:10 -0700

    Lodash partial is no longer needed. Match project style for arrow function arguments.

--- src/util/get-current-branch.js
+++ src/util/get-current-branch.js
@@ -1,5 +1,4 @@
-import partial from 'lodash/partial';
 import getCmdStdout from './get-cmd-stdout';
 const cmd = 'git rev-parse --abbrev-ref HEAD';
 
-export default (cwd) => partial(getCmdStdout, cmd, { cwd })();
+export default (cwd) => getCmdStdout(cmd, { cwd });
