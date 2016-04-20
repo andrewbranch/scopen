f33bcbd985368b0f4428c4d969100f0a158f67cc
Clay Miller <clay@smockle.com>
2016-04-20 11:21:06 -0700

    Match project style for arrow function arguments.

--- src/util/get-current-branch.js
+++ src/util/get-current-branch.js
@@ -1,4 +1,4 @@
 import getCmdStdout from './get-cmd-stdout';
 const cmd = 'git rev-parse --abbrev-ref HEAD';
 
-export default (cwd) => getCmdStdout(cmd, { cwd });
+export default cwd => getCmdStdout(cmd, { cwd });
