226bd4e52de609fda9dc9cb3a8e5c864d34675ca
Andrew Branch <andrewbranch@mail.com>
2016-04-19 22:03:10 -0700

    Set up logging correctly

--- src/scopen.js
+++ src/scopen.js
@@ -1,11 +1,19 @@
 import getCurrentBranch from './util/get-current-branch';
 import getRemoteURL from './util/get-remote-url';
+import setupLogger from './util/logger';
 
 export default program => {
-  const { remote, branch = getCurrentBranch() } = program;
+  const { verbosity, remote, branch = getCurrentBranch() } = program;
+  const logger = setupLogger(verbosity);
+  logger.debug('Initialized program');
+  logger.verbose('Options passed to scopen.js:', program);
+
   Promise.all([getRemoteURL(remote), branch]).then(([rem, br]) => {
-    console.log(rem, br);
+    logger.debug('Got remote:', rem);
+    logger.debug('Got branch:', br);
   }).catch(err => {
-    console.error(err.stack);
+    logger.error('Could not get either remote URL or branch. (Try --verbosity=debug or --verbosity=verbose for more info.)'); // eslint-disable-line max-len
+    logger.debug(err.message);
+    logger.verbose(err.stack);
   });
 };
--- src/util/logger.js
+++ src/util/logger.js
@@ -1,22 +1,24 @@
 import winston from 'winston';
 const testLevel = process.env.NODE_ENV === 'test' ? 'error' : null;
 
-export default (logLevel = 'info') => new winston.Logger({
-  transports: [new winston.transports.Console({ level: testLevel || logLevel })],
-  levels: {
+winston.addColors({
+  success: 'green',
+});
+
+export default (logLevel = 'info') => {
+  const logger = new winston.Logger({
+    transports: [new winston.transports.Console({ level: testLevel || logLevel })],
+  });
+
+  logger.cli();
+  logger.setLevels({
     error: 0,
     warn: 1,
     success: 2,
     info: 3,
     debug: 4,
     verbose: 5,
-  },
-  colors: {
-    error: 'red',
-    warn: 'yellow',
-    success: 'green',
-    info: 'white',
-    debug: 'white',
-    verboxe: 'white',
-  },
-});
+  });
+
+  return logger;
+};
