560b47982442c5f2cc30171373f4d1e9e7bf7115
Andrew Branch <andrewbranch@mail.com>
2016-04-19 20:20:39 -0700

    Scaffold command options with commander

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import program from 'commander';
+import pack from '../package.json';
+
+program
+  .version(pack.version)
+  .description(pack.description)
+  .usage('[options] <file>')
+  .option('-w, --with [cmd]', 'shell program used to open the URL [open]', 'open')
+  .option('-a, --application [app]', 'application option passed through to `open`')
+  .option('-b, --branch [branch]', 'git branch to view file [current branch]')
+  .option('-r, --remote [remote]', 'git remote to view file [origin]', 'origin')
+  .parse(process.argv);
+
+console.log(program);
